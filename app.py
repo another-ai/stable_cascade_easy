c01e6b4f53717002777e1add8021e536388356c4
another-ai <maxencry@gmail.com>
2024-02-14 20:19:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ def image_print_create(prompt,negative_prompt,random_seed,input_seed,width,heigh
     num_images_per_prompt = 1
 
     prior = StableCascadePriorPipeline.from_pretrained("stabilityai/stable-cascade-prior", torch_dtype=torch.bfloat16).to(device)
-
     prior.safety_checker = None
     prior.requires_safety_checker = False
 
