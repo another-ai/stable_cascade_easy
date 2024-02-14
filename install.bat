22032882d292676915bd1b1ad24a36f09e516b4f
another-ai <maxencry@gmail.com>
2024-02-15 00:54:23 +0100

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,6 @@
+git clone https://github.com/shiroppo/stable_cascade_easy
+cd stable_cascade_easy
+py -m venv venv
+call .\venv\Scripts\activate
+pip install -r requirements.txt
+pip install git+https://github.com/kashif/diffusers.git@wuerstchen-v3
