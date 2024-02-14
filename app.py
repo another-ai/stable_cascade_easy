b8c610236c3ae3518a573fce4905ac71c27ffd64
another-ai <maxencry@gmail.com>
2024-02-14 20:02:35 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,45 +1,72 @@
 import torch
 from diffusers import StableCascadeDecoderPipeline, StableCascadePriorPipeline
 import gc
+import gradio as gr
+import random
 
-device = "cuda"
-num_images_per_prompt = 1
+def image_print_create(prompt,negative_prompt,random_seed,input_seed,width,height,guidance_scale,num_inference_steps):
 
-prior = StableCascadePriorPipeline.from_pretrained("stabilityai/stable-cascade-prior", torch_dtype=torch.bfloat16).to(device)
+    device = "cuda"
+    num_images_per_prompt = 1
 
-prior.safety_checker = None
-prior.requires_safety_checker = False
+    prior = StableCascadePriorPipeline.from_pretrained("stabilityai/stable-cascade-prior", torch_dtype=torch.bfloat16).to(device)
 
-prompt = "a cat"
-negative_prompt = ""
+    prior.safety_checker = None
+    prior.requires_safety_checker = False
 
-prior_output = prior(
-    prompt=prompt,
-    width=1280,
-    height=1536,
-    negative_prompt=negative_prompt,
-    guidance_scale=4.0,
-    num_images_per_prompt=num_images_per_prompt,
-    num_inference_steps=20
-)
+    if prompt =="":
+        prompt = "a cat"
+    negative_prompt = ""
 
-del prior
-gc.collect()
-torch.cuda.empty_cache()
+    if random_seed:
+        input_seed = random.randint(0, 9999999999)
+    else:
+        input_seed = int(input_seed)
+    prior_output = prior(
+        prompt=prompt,
+        width=width,
+        height=height,
+        negative_prompt=negative_prompt,
+        guidance_scale=guidance_scale,
+        num_inference_steps=num_inference_steps,
+        num_images_per_prompt=num_images_per_prompt,
+    )
 
-decoder = StableCascadeDecoderPipeline.from_pretrained("stabilityai/stable-cascade",  torch_dtype=torch.float16).to(device)
-decoder.safety_checker = None
-decoder.requires_safety_checker = False
+    del prior
+    gc.collect()
+    torch.cuda.empty_cache()
 
-decoder_output = decoder(
-    image_embeddings=prior_output.image_embeddings.half(),
-    prompt=prompt,
-    negative_prompt=negative_prompt,
-    guidance_scale=0.0,
-    output_type="pil",
-    num_inference_steps=12
-).images[0].save("image.png")
+    decoder = StableCascadeDecoderPipeline.from_pretrained("stabilityai/stable-cascade",  torch_dtype=torch.float16).to(device)
+    decoder.safety_checker = None
+    decoder.requires_safety_checker = False
 
-# del decoder
-# gc.collect()
-# torch.cuda.empty_cache()
+    image = decoder(image_embeddings=prior_output.image_embeddings.half(),
+        prompt=prompt,
+        negative_prompt=negative_prompt,
+        guidance_scale=0.0,
+        output_type="pil",
+        num_inference_steps=12
+    ).images[0]
+
+    del decoder
+    gc.collect()
+    torch.cuda.empty_cache()
+
+    return image
+
+if __name__ == "__main__":
+
+    interface = gr.Interface(
+        fn=image_print_create,
+        inputs=[gr.Textbox(value="", lines=4, label="Prompt"),
+                gr.Textbox(value="", lines=4, label="Negative Prompt"),
+                gr.Checkbox(value=True, label="Random Seed"),
+                gr.Number(value=1234, label="Input Seed"),
+                gr.Number(value=768, label="Width",step=100),
+                gr.Number(value=1024, label="Height",step=100),
+                gr.Number(value=4, label="Guidance Scale",step=0.5),
+                gr.Number(value=20, label="Steps",step=1)],
+        outputs="image", 
+        live=False
+    )
+    interface.launch(share=False, inbrowser=True)
