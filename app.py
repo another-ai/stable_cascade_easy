5601d8820933e2396432f6413d855e29711fceb3
another-ai <maxencry@gmail.com>
2024-02-14 12:06:47 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+import torch
+from diffusers import StableCascadeDecoderPipeline, StableCascadePriorPipeline
+import gc
+
+device = "cuda"
+num_images_per_prompt = 1
+
+prior = StableCascadePriorPipeline.from_pretrained("stabilityai/stable-cascade-prior", torch_dtype=torch.bfloat16).to(device)
+
+prior.safety_checker = None
+prior.requires_safety_checker = False
+
+prompt = "a cat"
+negative_prompt = ""
+
+prior_output = prior(
+    prompt=prompt,
+    width=1280,
+    height=1536,
+    negative_prompt=negative_prompt,
+    guidance_scale=4.0,
+    num_images_per_prompt=num_images_per_prompt,
+    num_inference_steps=20
+)
+
+del prior
+gc.collect()
+torch.cuda.empty_cache()
+
+decoder = StableCascadeDecoderPipeline.from_pretrained("stabilityai/stable-cascade",  torch_dtype=torch.float16).to(device)
+decoder.safety_checker = None
+decoder.requires_safety_checker = False
+
+decoder_output = decoder(
+    image_embeddings=prior_output.image_embeddings.half(),
+    prompt=prompt,
+    negative_prompt=negative_prompt,
+    guidance_scale=0.0,
+    output_type="pil",
+    num_inference_steps=30
+).images[0].save("Image.png")
+
+# del decoder
+# gc.collect()
+# torch.cuda.empty_cache()
