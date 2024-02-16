c7e86c651112ba89096335d8afa9bd51eecb050a
another-ai <maxencry@gmail.com>
2024-02-16 13:10:27 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,4 +3,5 @@ cd stable_cascade_easy
 py -m venv venv
 call .\venv\Scripts\activate
 pip install -r requirements.txt
+pip install git+https://github.com/kashif/diffusers.git@a3dc21385b7386beb3dab3a9845962ede6765887 --force
 pause
