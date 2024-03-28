9480342742ca16be5a3cc1be24c1d379003ea242
another-ai <maxencry@gmail.com>
2024-03-28 20:14:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def generate_image(checkpoint_basename,checkpoint_prior,checkpoint_decoder,promp
                 prompt_input = prompt_input + ","
         banned_words = os.getenv("banned_words", "").split(",")
         import app_retnet
-        prompt = app_retnet.main_def(prompt_input=prompt_input, max_tokens=dynamic_prompt, DEVICE="cpu", banned_words=banned_words)
+        prompt = app_retnet.main_def(prompt_input=prompt_input, max_tokens=dynamic_prompt, DEVICE="cpu", banned_words=banned_words, prompt_chara=False)
         prompt = remove_duplicates(prompt)
         prompt = remove_last_comma(prompt)
     else:
